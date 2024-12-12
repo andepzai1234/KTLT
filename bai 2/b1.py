d958c5f23c18148691251f321cea694b93078f17
andepzai1234 <163100727+andepzai1234@users.noreply.github.com>
2024-12-12 15:03:34 +0700

    Create b1.py

---
+++ bai 2/b1.py
@@ -0,0 +1,11 @@
+print("Sinh viên:Tran Duc An")
+print("MSSV:215748020110273")
+print("####################")
+############################
+
+n1=int(input("enter n1 value"))
+n2=int(input("enter n2 value"))
+if n1>n2: print("n1 is big")
+else: print("n2 is big")
+
+    
