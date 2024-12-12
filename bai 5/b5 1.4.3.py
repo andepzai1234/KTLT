daf1bd7bad1c762a2e6d436113d2fca921828a80
andepzai1234 <163100727+andepzai1234@users.noreply.github.com>
2024-12-12 15:13:21 +0700

    Add files via upload

---
+++ bai 5/b5 1.4.3.py
@@ -0,0 +1,7 @@
+print("Sinh viên: Tran Duc An")
+print("MSSV: 215748020110273")
+print("####################")
+
+import numpy as np 
+x = np.arange(12, 38) 
+print(x) 
---
+++ bai 5/b5 1.4.4.py
@@ -0,0 +1,13 @@
+print("Sinh viên: Tran Duc An")
+print("MSSV: 215748020110273")
+print("####################")
+
+array = list(range(12, 38))  
+# In mảng ban đầu
+print("Mảng ban đầu:", array)
+
+#  Đảo ngược mảng
+reversed_array = array[::-1]
+
+#  In mảng sau khi đảo ngược
+print("Mảng đã đảo ngược:", reversed_array)
