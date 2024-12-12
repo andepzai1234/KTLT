e6800ea0d50d9d8c63ec117920b31c282370b641
andepzai1234 <163100727+andepzai1234@users.noreply.github.com>
2024-12-12 15:05:58 +0700

    Create b3 1.1.py

---
+++ bai3/b3 1.1.py
@@ -0,0 +1,11 @@
+print("Sinh viên:Tran Duc An")
+print("MSSV:215748020110273")
+print("####################")
+############################
+
+def sum(a, b):
+ print("sum = " + str(a + b))
+# tinh tong 2 so 4,5
+sum(4, 5)
+# tinh tong 2 so 3,7
+sum(3, 7)
