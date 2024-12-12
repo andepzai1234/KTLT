0ef350ed0fac5c65151077dd5c2b06d289bdb44f
andepzai1234 <163100727+andepzai1234@users.noreply.github.com>
2024-12-12 15:22:09 +0700

    Add files via upload

---
+++ bai 8/b8  1.4.2.py
@@ -0,0 +1,15 @@
+print("Sinh viên: Tran Duc An")
+print("MSSV: 215748020110273")
+print("####################")
+
+import turtle, random
+colors = ["red","green","blue","orange","purple","pink","yellow"]
+painter = turtle.Turtle()
+painter.pensize(3)
+for i in range(10):
+    color = random.choice(colors)
+    painter.pencolor(color)
+    painter.circle(100)
+    painter.right(30)
+    painter.left(60)
+    painter.setposition(0, 0)
---
+++ bai 8/b8  1.4.4.py
@@ -0,0 +1,19 @@
+print("Sinh viên: Tran Duc An")
+print("MSSV: 215748020110273")
+print("####################")
+import tkinter as tk
+
+# Tạo cửa sổ chính
+window = tk.Tk()
+
+# Đặt tiêu đề cho cửa sổ
+window.title("Cửa sổ đồ họa Tkinter")
+
+# Đặt kích thước cho cửa sổ (width x height)
+window.geometry("400x300")
+
+# Đặt một số thuộc tính cho cửa sổ (tuỳ chọn)
+window.configure(bg="lightblue")  # Thay đổi màu nền của cửa sổ
+
+# Hiển thị cửa sổ
+window.mainloop()
